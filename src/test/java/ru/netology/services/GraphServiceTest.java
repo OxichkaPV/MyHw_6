cf2c972a84114ce42e52ce61652b44d15a014590
oxanaP <oxichka@list.ru>
2024-06-23 10:11:47 +0700

    Initial commit

---
+++ src/test/java/ru/netology/services/GraphServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GraphServiceTest {
+    @Test
+    void numberOfWorkingMonthsInYear1() {
+        GraphService service = new GraphService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void numberOfWorkingMonthsInYear2() {
+        GraphService service = new GraphService();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+    }
+}
