0db70c7bd60d8f501126679a567d9542321a1a0b
oxanaP <oxichka@list.ru>
2024-06-23 10:36:04 +0700

    Add ParameterizedTest

--- src/test/java/ru/netology/services/GraphServiceTest.java
+++ src/test/java/ru/netology/services/GraphServiceTest.java
@@ -1,22 +1,16 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class GraphServiceTest {
-    @Test
-    void numberOfWorkingMonthsInYear1() {
-        GraphService service = new GraphService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        Assertions.assertEquals(expected, actual);
-    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
 
-    @Test
-    void numberOfWorkingMonthsInYear2() {
-        GraphService service = new GraphService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+    void ParameterTest(int income, int expenses, int threshold, int expected){
+        GraphService  service = new GraphService();
+        int actual = service.calculate(income,expenses,threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
